8c468777240c752184d62f3486b881f47890eebf
Kridsadakorn Chaichoompu <kridsadakorn.chaichoompu@gmail.com>
2019-09-13 13:45:10 +0200

    add bash scripts to test Matrics and runIntegration

---
+++ scripts/atac/run_integration_Scanorama.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source activate sc-clone
+
+METHOD=scanorama
+WORKDIR=/home/icb/chaichoompu/Group/workspace/Benchmarking_data_integration
+PYSCRIPT=${WORKDIR}/scripts/runIntegration.py
+INPUTFILE=${WORKDIR}/data/brain_atac_3datasets/merge_10x_CEMBA180312_3B_GSM3034638_bin_merged_filterRowCol_filterCountCell.h5ad
+OUTPUTFILE=${WORKDIR}/data/brain_atac_3datasets/merge_10x_CEMBA180312_3B_GSM3034638_bin_merged_filterRowCol_filterCountCell_${METHOD}.h5ad
+OUTPUTLOG=${WORKDIR}/data/brain_atac_3datasets/merge_10x_CEMBA180312_3B_GSM3034638_bin_merged_filterRowCol_filterCountCell_${METHOD}.txt
+${PYSCRIPT} -i ${INPUTFILE} -o ${OUTPUTFILE} -b batchname -v 0 -m ${METHOD} > ${OUTPUTLOG}
---
+++ scripts/atac/run_matrics_Scanorama_embed.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source activate sc-clone
+
+METHOD=Scanorama
+MODE=embed
+WORKDIR=/home/icb/chaichoompu/Group/workspace/Benchmarking_data_integration_matrics/Benchmarking_data_integration
+DATADIR=/home/icb/chaichoompu/Group/workspace/Benchmarking_data_integration
+PYSCRIPT=${WORKDIR}/scripts/metrics.py
+INPUTBEFORE=${DATADIR}/data/brain_atac_3datasets/merge_10x_CEMBA180312_3B_GSM3034638_bin_merged_top_var_feat_min100cells_regression_cell_labels.h5ad
+INPUTAFTER=${DATADIR}/data/brain_atac_3datasets/atac_brain_result_top_var_feat_${METHOD}.h5ad
+OUTPUTDIR=${DATADIR}/data/brain_atac_3datasets/metrics_${METHOD}_${MODE}
+mkdir ${OUTPUTDIR}
+${PYSCRIPT} --uncorrected ${INPUTBEFORE} --integrated ${INPUTAFTER} --output ${OUTPUTDIR} --batch_key batchname --label_key cell_type_consensus --type ${MODE}
