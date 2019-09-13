d333e5d101c02f8c193f774293575c6aa7eb2441
Kridsadakorn Chaichoompu <kridsadakorn.chaichoompu@gmail.com>
2019-09-13 14:08:18 +0200

    edit run_matrics*.sh

--- scripts/atac/run_matrics_BBKNN_knn.sh
+++ scripts/atac/run_matrics_BBKNN_knn.sh
@@ -10,5 +10,7 @@ PYSCRIPT=${WORKDIR}/scripts/metrics.py
 INPUTBEFORE=${DATADIR}/data/brain_atac_3datasets/merge_10x_CEMBA180312_3B_GSM3034638_bin_merged_top_var_feat_min100cells_regression_cell_labels.h5ad
 INPUTAFTER=${DATADIR}/data/brain_atac_3datasets/atac_brain_result_top_var_feat_${METHOD}.h5ad
 OUTPUTDIR=${DATADIR}/data/brain_atac_3datasets/metrics_${METHOD}_${MODE}
-mkdir ${OUTPUTDIR}
+if [ ! -d "$${OUTPUTDIR}" ]; then
+  mkdir ${OUTPUTDIR}
+fi
 ${PYSCRIPT} --uncorrected ${INPUTBEFORE} --integrated ${INPUTAFTER} --output ${OUTPUTDIR} --batch_key batchname --label_key cell_type_consensus --type ${MODE}
