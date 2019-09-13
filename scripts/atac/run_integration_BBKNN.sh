7d501e5dd8da1e761d840a28ba912ac271f36adc
Kridsadakorn Chaichoompu <kridsadakorn.chaichoompu@gmail.com>
2019-09-13 15:05:52 +0200

    fix path in bash scripts

--- scripts/atac/run_integration_BBKNN.sh
+++ scripts/atac/run_integration_BBKNN.sh
@@ -4,7 +4,7 @@ source activate sc-clone
 
 METHOD=bbknn
 WORKDIR=/home/icb/chaichoompu/Group/workspace/Benchmarking_data_integration
-PYSCRIPT=${WORKDIR}/scripts/runIntegration.py
+PYSCRIPT=${WORKDIR}_branch_ATAC/Benchmarking_data_integration/scripts/runIntegration.py
 INPUTFILE=${WORKDIR}/data/brain_atac_3datasets/merge_10x_CEMBA180312_3B_GSM3034638_bin_merged_filterRowCol_filterCountCell.h5ad
 OUTPUTFILE=${WORKDIR}/data/brain_atac_3datasets/merge_10x_CEMBA180312_3B_GSM3034638_bin_merged_filterRowCol_filterCountCell_${METHOD}.h5ad
 OUTPUTLOG=${WORKDIR}/data/brain_atac_3datasets/merge_10x_CEMBA180312_3B_GSM3034638_bin_merged_filterRowCol_filterCountCell_${METHOD}.txt
