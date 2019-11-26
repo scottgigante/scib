67c5f7fd1f9f851c4071bc70b8bb8989473495b9
Kris <1006124+kridsadakorn@users.noreply.github.com>
2019-11-26 20:27:19 +0100

    edit metrics stuff

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -2,6 +2,8 @@
 # coding: utf-8
 
 # To run the code from brach metric fixes
+import sys
+sys.path.remove('/home/icb/chaichoompu/Benchmarking_data_integration')
 sys.path.append('/storage/groups/ce01/workspace/Benchmarking_data_integration_branch_metric_fixes/Benchmarking_data_integration')
 
 import scanpy as sc
