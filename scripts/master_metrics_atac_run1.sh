0529067aaf44747ab2b873e098c6273982475dfd
Kris <1006124+kridsadakorn@users.noreply.github.com>
2019-11-26 13:40:57 +0100

    add slave metrics.py

--- scripts/master_metrics_atac_run1.sh
+++ scripts/master_metrics_atac_run1.sh
@@ -23,8 +23,8 @@ function run_all {
     FPREF=${FBASE%.*}
     NODE_SBATCH_LOG=${6}/${FPREF}_type${10}_metrics_log.txt
     NODE_SBATCH_ERR=${6}/${FPREF}_type${10}_metrics_err.txt
-    sbatch --partition=serial_fed28 --qos=usr_lmts --ntasks=1 --cpus-per-task=${2} --mem-per-cpu=${3} --time=1-00:00:00 --job-name=metric --output=${NODE_SBATCH_LOG} --error=${NODE_SBATCH_ERR} ${1}/slave_metrics.sh ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11}
-    echo sbatch --partition=serial_fed28 --qos=usr_lmts --ntasks=1 --cpus-per-task=${2} --mem-per-cpu=${3} --time=1-00:00:00 --job-name=metric --output=${NODE_SBATCH_LOG} --error=${NODE_SBATCH_ERR} ${1}/slave_metrics.sh ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11}
+    sbatch --partition=icb_cpu --qos=icb_stndrd --ntasks=1 --cpus-per-task=${2} --mem-per-cpu=${3} --time=1-00:00:00 --job-name=met --output=${NODE_SBATCH_LOG} --error=${NODE_SBATCH_ERR} ${1}/slave_metrics.sh ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11}
+    echo sbatch --partition=icb_cpu --qos=icb_stndrd --ntasks=1 --cpus-per-task=${2} --mem-per-cpu=${3} --time=1-00:00:00 --job-name=met --output=${NODE_SBATCH_LOG} --error=${NODE_SBATCH_ERR} ${1}/slave_metrics.sh ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11}
 
     # add a bit of delay, otherwise it will be too overloaded for Slurm
     sleep 0.3
@@ -33,22 +33,23 @@ function run_all {
                 
 # Run settings
 # Please use the full path
-#bbknn_knn scanorama_full scanorama_embed harmony_embed trvae_full trvae_embed
+#bbknn_knn scanorama_full scanorama_embed harmony_embed trvae_full trvae_embed mnn_full
 
-for PARAM in bbknn_knn scanorama_full scanorama_embed harmony_embed
+for PARAM in scanorama_full scanorama_embed harmony_embed trvae_full trvae_embed mnn_full
 do
-    METHOD=${PARAM%_*}
-    TYPE=${PARAM##*_}
-    
-    INPUTFILE=/storage/groups/ce01/workspace/Benchmarking_data_integration/data/brain_atac_3datasets/merge_10x_CEMBA180312_3B_GSM3034638_bin_merged_filterRowCol_filterCountCell.h5ad
-    INTEGRATIONFILE=/storage/groups/ce01/workspace/Benchmarking_data_integration/data/brain_atac_3datasets/output/merge_10x_CEMBA180312_3B_GSM3034638_bin_merged_filterRowCol_filterCountCell_${METHOD}.h5ad
-    OUTDIR=/storage/groups/ce01/workspace/Benchmarking_data_integration/data/brain_atac_3datasets
-    BATCH=batchname
-    CELLTYPE=broad_cell_label
-    ORGANISM=human
-    HVGS=0
-    run_all ${SCRIPTPATH} ${CPU} ${RAMperCPU} ${INPUTFILE} ${INTEGRATIONFILE} ${OUTDIR} ${BATCH} ${CELLTYPE} ${ORGANISM} ${TYPE} ${HVGS}
-
+    for HVGS in 0
+    do
+        METHOD=${PARAM%_*}
+        TYPE=${PARAM##*_}
+        
+        INPUTFILE=/storage/groups/ce01/workspace/Benchmarking_data_integration/data/brain_atac_3datasets/merge_10x_CEMBA180312_3B_GSM3034638_bin_merged_top_var_feat_min500cells_regression_Seurat_cell_labels.h5ad
+        INTEGRATIONFILE=/storage/groups/ce01/workspace/Benchmarking_data_integration/data/brain_atac_3datasets/output/merge_10x_CEMBA180312_3B_GSM3034638_bin_merged_top_var_feat_min500cells_regression_Seurat_cell_labels_${METHOD}_hvg${HVGS}.h5ad
+        OUTDIR=/storage/groups/ce01/workspace/Benchmarking_data_integration/data/brain_atac_3datasets
+        BATCH=batchname
+        CELLTYPE=cell_type_consensus
+        ORGANISM=human
+        run_all ${SCRIPTPATH} ${CPU} ${RAMperCPU} ${INPUTFILE} ${INTEGRATIONFILE} ${OUTDIR} ${BATCH} ${CELLTYPE} ${ORGANISM} ${TYPE} ${HVGS}
+    done
 done
 
 # All temporary files and directories won't be removed since we can reuse them again and again
