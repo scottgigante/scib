a6b2dfcd857b4874b4942084b47b677825d4556e
Git-admin <kridsadakorn.chaichoompu@ulg.ac.be>
2020-01-21 19:21:47 +0100

    runintegration added

--- scripts/master_integration_mnn_atac.sh
+++ scripts/master_integration_mnn_atac.sh
@@ -4,6 +4,7 @@
 SCRIPTPATH=/home/icb/chaichoompu/Group/workspace/Benchmarking_data_integration_branch_ATAC/Benchmarking_data_integration/scripts
 CPU=24
 RAMperCPU=16000
+RERUN=0
 
 #To call run_all
 #$1= path of script or SCRIPTPATH
@@ -13,6 +14,7 @@ RAMperCPU=16000
 #$5= batch column name or BATCH
 #$6= number of HVGs or HVGS
 #$7= output path or OUTDIR, the directory needs to existed, the output files will be saved under $OUTDIR/output
+#$8= rerun = 1, otherwise not
  
 function run_all {
     for METHOD in mnn
@@ -21,7 +23,7 @@ function run_all {
         FPREF=${FBASE%.*}
         NODE_SBATCH_LOG=${7}/${FPREF}_${METHOD}_hvg${6}_log.txt
         NODE_SBATCH_ERR=${7}/${FPREF}_${METHOD}_hvg${6}_err.txt
-        sbatch --partition=icb_rstrct --qos=icb_rstrct --ntasks=1 --cpus-per-task=${2} --mem-per-cpu=${3} --time=4-00:00:00 --job-name=scib --output=${NODE_SBATCH_LOG} --error=${NODE_SBATCH_ERR} ${1}/slave_integration.sh ${4} ${5} ${6} ${7} ${METHOD}
+        sbatch --partition=icb_rstrct --qos=icb_rstrct --ntasks=1 --cpus-per-task=${2} --mem-per-cpu=${3} --time=4-00:00:00 --job-name=scib --output=${NODE_SBATCH_LOG} --error=${NODE_SBATCH_ERR} ${1}/slave_integration.sh ${4} ${5} ${6} ${7} ${METHOD} ${8}
 
         # add a bit of delay, otherwise it will be too overloaded for Slurm
         sleep 0.3
@@ -42,7 +44,7 @@ do
    BATCH=`echo "$i"|cut -d'|' -f2`
    OUTDIR=`echo "$i"|cut -d'|' -f3`
    HVGS=0
-   run_all ${SCRIPTPATH} ${CPU} ${RAMperCPU} ${INPUTFILE} ${BATCH} ${HVGS} ${OUTDIR}
+   run_all ${SCRIPTPATH} ${CPU} ${RAMperCPU} ${INPUTFILE} ${BATCH} ${HVGS} ${OUTDIR} ${RERUN}
 done
 
 # All temporary files and directories won't be removed since we can reuse them again and again
